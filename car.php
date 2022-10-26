d240495d50873aa14048549a52d1690c22a2df76
dcosovan <david.cosovan@leviatan.ro>
2022-10-26 17:42:23 +0300

    Part1

--- car.php
+++ car.php
@@ -1,2 +1,40 @@
 <?php
-echo 'hello';
+class Car
+{
+    public  $color;
+    public  $weight;
+    private  $year;
+    private $availableColours=['red','green','blue'];
+
+    public function setYear($year)
+    {
+        $this->year = $year;
+    }
+
+    public function getYear()
+    {
+        return $this->year;
+    }
+
+    public function setColor($color)
+    {
+       if(in_array($color,$this->availableColours)){
+        $this->color=$color;
+       }
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+}
+
+$myCar = new Car();
+$myCar->setYear(2010);
+echo $myCar->getYear();
+$myCar->setColor('red');
+echo $myCar->getColor();
+// echo '<pre>';
+// var_dump($myCar);
+// echo '</pre>';
